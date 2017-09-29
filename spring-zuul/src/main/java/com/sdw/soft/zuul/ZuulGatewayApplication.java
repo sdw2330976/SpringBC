9a9c7683ccb280481310646cdf609de9afabc38c
sdw2330976 <sydhappy123@163.com>
2017-09-29 23:21:13 +0800

    zuul support

---
+++ spring-zuul/src/main/java/com/sdw/soft/zuul/ZuulGatewayApplication.java
@@ -0,0 +1,20 @@
+package com.sdw.soft.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by shangyd on 2017/9/29.
+ */
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class ZuulGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
+
+}
