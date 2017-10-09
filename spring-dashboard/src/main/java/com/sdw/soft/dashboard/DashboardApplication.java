143150d9268add23ef7387ba9e9eeba44a26699f
sdw2330976 <sydhappy123@163.com>
2017-10-09 19:14:37 +0800

    hystrix dashboard support

---
+++ spring-dashboard/src/main/java/com/sdw/soft/dashboard/DashboardApplication.java
@@ -0,0 +1,23 @@
+package com.sdw.soft.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * Created by shangyd on 2017/10/9.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+public class DashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashboardApplication.class, args);
+    }
+}
