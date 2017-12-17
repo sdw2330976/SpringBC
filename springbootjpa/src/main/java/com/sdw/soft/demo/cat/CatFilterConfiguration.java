f52d92d3775734bc59e81eb6664958e66ea6bef9
sdw2330976 <sydhappy123@163.com>
2017-12-17 22:13:57 +0800

    integrate CAT

---
+++ springbootjpa/src/main/java/com/sdw/soft/demo/cat/CatFilterConfiguration.java
@@ -0,0 +1,24 @@
+package com.sdw.soft.demo.cat;
+
+import com.dianping.cat.servlet.CatFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by shangyd on 2017/12/17.
+ */
+@Configuration
+public class CatFilterConfiguration {
+
+    @Bean
+    public FilterRegistrationBean catFilter() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        CatFilter catFilter = new CatFilter();
+        registrationBean.setFilter(catFilter);
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setName("cat-Filter");
+        registrationBean.setOrder(2);
+        return registrationBean;
+    }
+}
